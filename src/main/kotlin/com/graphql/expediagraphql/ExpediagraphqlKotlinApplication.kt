c2752febf3f46f81aef3365ba13f46890aa62941
Sameer Kulkarni <s.kulkarni@tignum.com>
2021-12-29 18:39:21 +0100

    add First commit

---
+++ src/main/kotlin/com/graphql/expediagraphql/ExpediagraphqlKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.graphql.expediagraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExpediagraphqlKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExpediagraphqlKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/graphql/expediagraphql/ExpediagraphqlKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.graphql.expediagraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExpediagraphqlKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
