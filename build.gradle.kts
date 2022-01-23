dfad902a598434f7852170f2b522f8310a01c30f
Sameer Kulkarni <s.kulkarni@tignum.com>
2022-01-23 20:21:39 +0100

    fix: downgrade H2

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("com.expediagroup:graphql-kotlin-hooks-provider:5.3.2")
     implementation("org.postgresql:postgresql:42.3.1")
 
-    runtimeOnly("com.h2database:h2:2.1.210")
+    runtimeOnly("com.h2database:h2:1.4.195")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.3")
     testImplementation("io.projectreactor:reactor-test:3.4.14")
 }
